dd8c70b2e3611d77368b4aea59c03ac1976f5aee
Kai Lueke <kailuke@microsoft.com>
2024-01-22 14:13:20 +0100

    generators: Don't write files other than unit files
    Systemd now executes generators in a sandbox to make sure they only
    generate units and don't have other side effects:
    https://github.com/systemd/systemd/blob/166bcaf95fbdd036f99705d2204138c36eb95836/NEWS#L1583
    
    Port our generator usage to comply with the new behavior so that we can
    update our systemd version. Move the Ignition preparations into the
    ignition-setup script, setup the fsck overwrite unit at install time,
    and remove the mkdir calls because Mount units will do this when needed.

--- dracut/30ignition/ignition-setup.sh
+++ dracut/30ignition/ignition-setup.sh
@@ -10,10 +10,16 @@ normal)
     mount /dev/disk/by-label/OEM "${src}"
     # retry-umount may not be necessary, but be cautious
     trap 'retry-umount "${src}"' EXIT
+    # Workaround, "chmod" is not available
+    cp -a /bin/cat /bin/is-live-image
+    printf '#!/bin/sh\nexit 1\n' > /bin/is-live-image
     ;;
 pxe)
     # OEM directory in the initramfs itself
     src=/oem
+    # Workaround, "chmod" is not available
+    cp -a /bin/cat /bin/is-live-image
+    printf '#!/bin/sh\nexit 0\n' > /bin/is-live-image
     ;;
 *)
     echo "Usage: $0 {normal|pxe}" >&2
@@ -29,3 +35,41 @@ fi
 if [[ -e "${src}/config.ign" ]]; then
     cp "${src}/config.ign" "${dst}/user.ign"
 fi
+
+cmdline=( $(</proc/cmdline) )
+cmdline_arg() {
+    local name="$1" value="$2"
+    for arg in "${cmdline[@]}"; do
+        if [[ "${arg%%=*}" == "${name}" ]]; then
+            value="${arg#*=}"
+        fi
+    done
+    echo "${value}"
+}
+
+oem_id=metal
+if [[ $(systemd-detect-virt || true) =~ ^(kvm|qemu)$ ]]; then
+    oem_id=qemu
+fi
+
+oem_cmdline="$(cmdline_arg flatcar.oem.id ${oem_id})"
+if [[ "${oem_id}" = "${oem_cmdline}" ]]; then
+    oem_cmdline="$(cmdline_arg coreos.oem.id ${oem_id})"
+fi
+
+# Ignition changed the platform name to "aws"
+if [ "${oem_cmdline}" = "ec2" ]; then
+  oem_cmdline="aws"
+fi
+
+# Ignition changed the platform name to "gcp"
+if [ "${oem_cmdline}" = "gce" ]; then
+  oem_cmdline="gcp"
+fi
+
+# To maintain compatibility with eventual legacy 'flatcar.oem.id=pxe'
+if [ "${oem_cmdline}" = "pxe" ]; then
+  oem_cmdline="metal"
+fi
+
+{ echo "OEM_ID=${oem_cmdline}" ; echo "PLATFORM_ID=${oem_cmdline}" ; } > /run/ignition.env
