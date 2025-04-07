8f808c1a08ff474c9cff5c1a016e4c607117a488
James Le Cuirot <jlecuirot@microsoft.com>
2025-04-07 18:40:55 +0100

    flatcat-debloat: Add module to trim the initrd more aggressively
    Signed-off-by: James Le Cuirot <jlecuirot@microsoft.com>

---
+++ dracut/99flatcar-debloat/module-setup.sh
@@ -0,0 +1,21 @@
+config() {
+    # gcrypt: Only needed for systemd-journal's FSS feature.
+    # lz4: Flatcar has never needed this for the journal or coredumps.
+    omit_dlopen_features+=" libsystemd-shared-*.so:gcrypt,lz4 "
+}
+
+# shellcheck disable=SC2064
+trap "$(shopt -p extglob)" RETURN
+shopt -q -s extglob
+
+install() {
+    # shellcheck disable=SC2064
+    trap "$(shopt -p extglob)" RETURN
+    shopt -q -s extglob
+
+    # Remove the NSS modules we don't need.
+    rm "${initdir}"/usr/lib*/libnss_!(dns|files|myhostname|resolve|systemd).so*
+
+    # We maybe should include this, but more work is needed for compliance.
+    rm "${initdir}"/usr/lib*/ossl-modules/fips.so
+}
