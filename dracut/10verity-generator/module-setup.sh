b0d2de8e9325be198ca6c75e055f059e5ac6d56b
Kai Lueke <kailuke@microsoft.com>
2025-10-01 20:01:02 +0900

    Remove unused generator code
    The minimal initrd now does the verity setup and /sysusr mounting and we
    can delete parts of the generator code.
    Delete the unused code but still keep support for "usr=" instead of
    "mount.usr=" cmdline entries. We could decide to remove this later to
    get rid of a few more things in the generators. Still, /sysroot(/usr)
    setup will be needed. For "mount.usr=" the upstream systemd generator
    handles this but there's still a bit of code to customize the mount args
    for it. The verity generator gets deleted fully and if we later want to
    use it again, we can use the upstream one with some cmdline
    preprocessing.
    
    Signed-off-by: Kai Lueke <kailuke@microsoft.com>

--- dracut/10verity-generator/module-setup.sh
+++ dracut/10verity-generator/module-setup.sh
@@ -3,14 +3,10 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 depends() {
-    echo systemd dm
+    echo systemd
 }
 
 install() {
-    inst_multiple veritysetup e2size systemd-escape tr
-    inst_simple "$moddir/verity-generator" \
-        "$systemdutildir/system-generators/verity-generator"
-
     # Device units default to a finite JobRunningTimeout, which would cause
     # dev-mapper-usr.device to fail if its dependencies took too long to
     # start. This was causing boot failures when Ignition was asked to
